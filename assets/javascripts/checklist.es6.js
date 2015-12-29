680c709511a0f660c2c45d312447d4c388f3e0c7
tim <timnakamura@gmail.com>
2015-12-29 16:11:49 -0700

    export

--- assets/javascripts/checklist.es6.js
+++ assets/javascripts/checklist.es6.js
@@ -1,5 +1,5 @@
-import PostView from "discourse/views/post";
-import Post from 'discourse/models/post';
+var PostView = require('discourse/views/post').default,
+    Post = require('discourse/models/post').default;
 
 PostView.reopen({
   createChecklistUI: function($post) {
