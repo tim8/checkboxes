425bc33122f72de362de84725ca4482b74d660fb
tim <timnakamura@gmail.com>
2015-12-29 11:20:39 -0700

    css

--- plugin.rb
+++ plugin.rb
@@ -2,12 +2,6 @@
 # version: 2
 # authors: root
 
-register_asset "javascripts/discourse/checklist_ui.js" :server_side
+register_asset "javascripts/discourse/checklist_ui.js", :server_side
 
-register_css <<CSS
-
-span.chcklst-stroked {
-	text-decoration: line-through;
-}
-
-CSS
+register_asset "stylesheets/checklist.scss"
