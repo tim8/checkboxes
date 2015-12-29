c1eb20d0946cb3ab2076cdc8be4cb58c2613f606
tim <timnakamura@gmail.com>
2015-12-29 14:52:32 -0700

    export

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # name: volunteer-boxes
 # version: 2
 # authors: root
-register_asset "javascripts/discourse/initializers/checklist.js.es6"
+register_asset "javascripts/discourse/checklist.js", :server_side
 register_asset "javascripts/discourse/checklist_dialect.js", :server_side
 
 register_asset "stylesheets/checklist.scss"
