bb41c7547d0486ed4311c1e37a898743ab4653a4
tim <timnakamura@gmail.com>
2015-12-29 10:50:48 -0700

    update

--- assets/javascripts/checklist_ui.js
+++ assets/javascripts/checklist_ui.js
@@ -1,43 +1,49 @@
-Discourse.PostView.reopen({
-  createChecklistUI: function($post) {
+import PostView from "discourse/views/post";
+import Post from 'discourse/models/post';
 
-    if (!this.get('post').can_edit) { return };
+export default {
+  name: 'checklist',
+  initialize: function(container) {
+      PostView.reopen({
+        createChecklistUI: function($post) {
+          if (!this.get('post.can_edit')) { return };
 
-    var boxes = $post.find(".chcklst-box"),
-        view = this.get('post'),
-        user = Discourse.User.currentProp('username');
+          var boxes = $post.find(".chcklst-box"),
+            viewPost = this.get('post');
+            user = Discourse.User.currentProp('username');
 
-    boxes.each(function(idx, val) {
-      $(val).click(function(ev) {
-        var elem = $(ev.currentTarget),
-            new_value = elem.hasClass("checked") ? "[ ] #VolunteerNeeded": "[*] @" + user,
-            poller = Discourse.ajax("/posts/" + view.id + ".json");
+          boxes.each(function(idx, val) {
+            $(val).click(function(ev) {
+              var elem = $(ev.currentTarget),
+                new_value = elem.hasClass("checked") ? "[ ] #VolunteerNeeded": "[*] @" + user,
 
-        elem.after('<i class="fa fa-spinner fa-spin"></i>');
-        elem.hide();
+              elem.after('<i class="fa fa-spinner fa-spin"></i>');
+              elem.hide();
 
-        poller.then(function(result) {
-          var nth = -1, // make the first run go to index = 0
-              new_raw = result.raw.replace(/(\[([\ \_\-\x\*]?)\])(\s((@|#)\w+))?/g, function(match, args, offset) {
-                nth += 1;
-                return nth == idx ? new_value : match;
+              var postId = viewPost.get('id');
+              Discourse.ajax("/posts/" + postId, { type: 'GET', cache: false }).then(function(result) {
+                var nth = -1, // make the first run go to index = 0
+                  new_raw = result.raw.replace(/(\[([\ \_\-\x\*]?)\])(\s((@|#)\w+))?/g, function(match, args, offset) {
+                    nth += 1;
+                    return nth == idx ? new_value : match;
+                  });
+
+                var props = {
+                  raw: new_raw,
+                  edit_reason: 'volunteered',
+                  cooked: Discourse.Markdown.cook(new_raw)
+                };
+                viewPost.save(props);
               });
-      Discourse.ajax("/posts/" + view.id, {
-        type: "PUT",
-        data: {
-          "post[raw]" : new_raw,
-          "post[edit_reason]" : "volunteered"
-        }
+            });
+          });
+
+          // confirm the feature is enabled by showing the click-ability
+          boxes.css({"cursor": "pointer"});
+        }.on('postViewInserted', 'postViewUpdated'),
+
+        destroyChecklistUI: function() {
+        }.on('willClearRender')
       });
-        });
-      });
-    });
-
-    // confirm the feature is enabled by showing the click-ability
-    boxes.css({"cursor": "pointer"});
-
-  }.on('postViewInserted', 'postViewUpdated'),
-
-  destroyChecklistUI: function() {
-  }.on('willClearRender')
-});
+  }
+};
