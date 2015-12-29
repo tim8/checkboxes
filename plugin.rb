606a785ef8709b5d9b45c235c87523df6fd25746
tim <timnakamura@gmail.com>
2015-12-29 11:24:32 -0700

    name

--- plugin.rb
+++ plugin.rb
@@ -2,6 +2,6 @@
 # version: 2
 # authors: root
 
-register_asset "javascripts/discourse/checklist_ui.js", :server_side
+register_asset "javascripts/discourse/checklist_dialect.js", :server_side
 
 register_asset "stylesheets/checklist.scss"
