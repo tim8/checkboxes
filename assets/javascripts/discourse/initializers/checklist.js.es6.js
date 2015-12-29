025a44a13b00d08f4418286b6cd1fe621e05ec1b
tim <timnakamura@gmail.com>
2015-12-29 14:11:22 -0700

    export

--- assets/javascripts/discourse/initializers/checklist.js.es6.js
+++ assets/javascripts/discourse/initializers/checklist.js.es6.js
@@ -1,47 +1,45 @@
 import PostView from "discourse/views/post";
 import Post from 'discourse/models/post';
 
-export default {
-      PostView.reopen({
-        createChecklistUI: function($post) {
-          if (!this.get('post.can_edit')) { return };
+PostView.reopen({
+  createChecklistUI: function($post) {
+    if (!this.get('post.can_edit')) { return };
 
-          var boxes = $post.find(".chcklst-box"),
-            viewPost = this.get('post'),
-            user = Discourse.User.currentProp('username');
-            console.log(user);
+    var boxes = $post.find(".chcklst-box"),
+      viewPost = this.get('post'),
+      user = Discourse.User.currentProp('username');
+      console.log(user);
 
-          boxes.each(function(idx, val) {
-            $(val).click(function(ev) {
-              var elem = $(ev.currentTarget),
-                new_value = elem.hasClass("checked") ? "[ ] #VolunteerNeeded": "[*] @" + user;
+    boxes.each(function(idx, val) {
+      $(val).click(function(ev) {
+        var elem = $(ev.currentTarget),
+          new_value = elem.hasClass("checked") ? "[ ] #VolunteerNeeded": "[*] @" + user;
 
-              elem.after('<i class="fa fa-spinner fa-spin"></i>');
-              elem.hide();
+        elem.after('<i class="fa fa-spinner fa-spin"></i>');
+        elem.hide();
 
-              var postId = viewPost.get('id');
-              Discourse.ajax("/posts/" + postId, { type: 'GET', cache: false }).then(function(result) {
-                var nth = -1, // make the first run go to index = 0
-                  new_raw = result.raw.replace(/\[([\ \_\-\x\*]?)\]/g, function(match, args, offset) {
-                    nth += 1;
-                    return nth == idx ? new_value : match;
-                  });
-
-                var props = {
-                  raw: new_raw,
-                  edit_reason: 'volunteered',
-                  cooked: Discourse.Markdown.cook(new_raw)
-                };
-                viewPost.save(props);
-              });
+        var postId = viewPost.get('id');
+        Discourse.ajax("/posts/" + postId, { type: 'GET', cache: false }).then(function(result) {
+          var nth = -1, // make the first run go to index = 0
+            new_raw = result.raw.replace(/\[([\ \_\-\x\*]?)\]/g, function(match, args, offset) {
+              nth += 1;
+              return nth == idx ? new_value : match;
             });
-          });
 
-          // confirm the feature is enabled by showing the click-ability
-          boxes.css({"cursor": "pointer"});
-        }.on('postViewInserted', 'postViewUpdated'),
-
-        destroyChecklistUI: function() {
-        }.on('willClearRender')
+          var props = {
+            raw: new_raw,
+            edit_reason: 'volunteered',
+            cooked: Discourse.Markdown.cook(new_raw)
+          };
+          viewPost.save(props);
+        });
       });
-};
+    });
+
+    // confirm the feature is enabled by showing the click-ability
+    boxes.css({"cursor": "pointer"});
+  }.on('postViewInserted', 'postViewUpdated'),
+
+  destroyChecklistUI: function() {
+  }.on('willClearRender')
+});
